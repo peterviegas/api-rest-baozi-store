b7f35baf170266caf062af269ef523d8e3d8bcd5
Peter <peterviegas@outlook.com>
2026-03-20 18:56:24 +0000

    start: configuration github

---
+++ src/main/java/com/baozi_store/BaoziStoreApplication.java
@@ -0,0 +1,13 @@
+package com.baozi_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoziStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoziStoreApplication.class, args);
+	}
+
+}
